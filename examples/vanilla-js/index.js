3d92a03748cd1faad28726f52c6d71083fb805a5
Giulia <56300363+GiuliaFerraioli@users.noreply.github.com>
2025-08-18 16:18:48 +0100

    Merge main scene and converter + add buttons

--- examples/vanilla-js/index.js
+++ examples/vanilla-js/index.js
@@ -9,26 +9,70 @@ const scene = new SPLAT.Scene();
 const camera = new SPLAT.Camera();
 const controls = new SPLAT.OrbitControls(camera, canvas);
 
-async function main() {
-    const url = "https://huggingface.co/datasets/dylanebert/3dgs/resolve/main/bonsai/bonsai-7k-mini.splat";
-    await SPLAT.Loader.LoadAsync(url, scene, (progress) => (progressIndicator.value = progress * 100));
+const format = "";
+
+async function loadFromURL(url) {
+    progressDialog.show();
+    if (url.endsWith(".splat")) {
+        await SPLAT.Loader.LoadAsync(url, scene, (progress) => (progressIndicator.value = progress * 100));
+    } else if (url.endsWith(".ply")) {
+        await SPLAT.PLYLoader.LoadAsync(url, scene, (progress) => (progressIndicator.value = progress * 100), format);
+        scene.saveToFile(url.split("/").pop()?.replace(".ply", ".splat"));
+    }
     progressDialog.close();
+}
 
-    const handleResize = () => {
-        renderer.setSize(canvas.clientWidth, canvas.clientHeight);
-    };
+let loading = false;
+async function selectFile(file) {
+    if (loading) return;
+    loading = true;
 
-    const frame = () => {
-        controls.update();
-        renderer.render(scene, camera);
+    if (file.name.endsWith(".splat")) {
+        await SPLAT.Loader.LoadFromFileAsync(file, scene, (progress) => (progressIndicator.value = progress * 100));
+    } else if (file.name.endsWith(".ply")) {
+        await SPLAT.PLYLoader.LoadFromFileAsync(file, scene, (progress) => (progressIndicator.value = progress * 100), format);
+        scene.saveToFile(file.name.replace(".ply", ".splat"));
+    }
 
-        requestAnimationFrame(frame);
-    };
+    loading = false;
+}
 
-    handleResize();
+document.addEventListener("drop", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (e.dataTransfer?.files.length) selectFile(e.dataTransfer.files[0]);
+});
+document.addEventListener("dragover", (e) => e.preventDefault());
+
+
+function renderLoop() {
+    controls.update();
+    renderer.render(scene, camera);
+    requestAnimationFrame(renderLoop);
+}
+
+
+const btnSplat = document.getElementById("load-splat");
+const btnPLY = document.getElementById("load-ply");
+
+btnSplat.addEventListener("click", () => {
+    const url = "https://huggingface.co/datasets/dylanebert/3dgs/resolve/main/bonsai/bonsai-7k-mini.splat";
+    loadFromURL(url);
+});
+
+btnPLY.addEventListener("click", () => {
+    const url = "https://huggingface.co/datasets/dylanebert/3dgs/resolve/main/bonsai/point_cloud/iteration_7000/point_cloud.ply";
+    loadFromURL(url);
+});
+
+
+function main() {
+    renderLoop();
+
+    const handleResize = () => renderer.setSize(canvas.clientWidth, canvas.clientHeight);
     window.addEventListener("resize", handleResize);
-
-    requestAnimationFrame(frame);
+    handleResize();
 }
 
 main();
+
